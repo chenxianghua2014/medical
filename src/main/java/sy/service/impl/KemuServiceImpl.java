7a16826277fc037de6232e4b3a78c3c0dfc40aa3
wlq <823610227@qq.com>
2013-07-24 21:28:10 +0800

    增加科目支出后的更新数据汇总表

--- src/main/java/sy/service/impl/KemuServiceImpl.java
+++ src/main/java/sy/service/impl/KemuServiceImpl.java
@@ -1,21 +1,24 @@
 package sy.service.impl;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 import org.apache.struts2.ServletActionContext;
+import org.hibernate.Query;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
 import sy.dao.BaseDaoI;
 import sy.model.Tkemu;
 import sy.model.Tmenu;
+import sy.model.Tpay;
 import sy.model.Tproject;
+import sy.model.Tuser;
 import sy.pageModel.DataGrid;
 import sy.pageModel.Kemu;
 import sy.pageModel.SessionInfo;
@@ -27,7 +30,9 @@ public class KemuServiceImpl extends BaseServiceImpl implements KemuServiceI {
 
 	private BaseDaoI<Tkemu> kemuDao;
 	private BaseDaoI<Tproject> projectDao;
-    
+	private BaseDaoI<Tuser> userDao;
+	private BaseDaoI<Tpay> payDao;
+	
 	public BaseDaoI<Tkemu> getKemuDao() {
 		return kemuDao;
 	}
@@ -45,7 +50,18 @@ public class KemuServiceImpl extends BaseServiceImpl implements KemuServiceI {
 	public void setProjectDao(BaseDaoI<Tproject> projectDao) {
 		this.projectDao = projectDao;
 	}
+	
+	@Autowired
+	public void setUserDao(BaseDaoI<Tuser> userDao) {
+		this.userDao = userDao;
+	}
+	
+	@Autowired
+	public void setPayDao(BaseDaoI<Tpay> payDao) {
+		this.payDao = payDao;
+	}
 
+	
 	@Transactional(propagation = Propagation.SUPPORTS)
 	public DataGrid datagrid(Kemu kemu) {
 		DataGrid j = new DataGrid();
@@ -94,9 +110,8 @@ public class KemuServiceImpl extends BaseServiceImpl implements KemuServiceI {
 		}
 		SessionInfo sessionInfo = (SessionInfo) ServletActionContext.getRequest().getSession().getAttribute(ResourceUtil.getSessionInfoName());
 		String cprojectid  = sessionInfo.getLoginName();
-		kemu.setCprojectid(cprojectid);	
-		
-		Tkemu t = new Tkemu();	
+		kemu.setCprojectid(cprojectid);			
+		Tkemu t = new Tkemu();
 		BeanUtils.copyProperties(kemu, t);
 		kemuDao.save(t);
 	}
@@ -115,7 +130,7 @@ public class KemuServiceImpl extends BaseServiceImpl implements KemuServiceI {
 				if (t != null) {
 					kemuDao.delete(t);
 				}
-			}
+			}			
 		}
 	}
 
@@ -124,4 +139,43 @@ public class KemuServiceImpl extends BaseServiceImpl implements KemuServiceI {
 		return menu;
 	}
 
+	public Tkemu updatepay(Kemu kemu) {
+		if (kemu.getCid() == null || kemu.getCid().trim().equals("")) {
+			kemu.setCid(UUID.randomUUID().toString());
+		}
+		SessionInfo sessionInfo = (SessionInfo) ServletActionContext.getRequest().getSession().getAttribute(ResourceUtil.getSessionInfoName());
+		String username  = sessionInfo.getLoginName();
+		String ccourse=kemu.getCcourse();
+		String hql1="select new Tuser(t.cid) from Tuser t where cname=? ";//获取用户id		
+		List<Tuser> t = userDao.find(hql1,new Object[] {username});
+		String cfuid = " ";
+		Tuser tu = t.get(0);
+		cfuid= tu.getCid();
+	    
+		String hql2="select sum(t.cmoney) as t from Tkemu t where cprojectid=? and ccourse=?";//获取sum	
+		List<Double> list = kemuDao.finds(hql2, new Object[] {username,ccourse});
+		Double sum = list.get(0);			
+		cfuid = "'" + cfuid +"'";
+		ccourse = "'" + ccourse +"'";
+		
+		String hql3="update Tpay tpay set tpay.ccost=" +sum+
+				" where tpay.cfuid=" +cfuid+
+				" and tpay.cname=" +ccourse;
+		payDao.executeHql(hql3);
+		/*String hql3="update Tpay tpay set tpay.ccost=?  where tpay.cfuid= ? and tpay.cname=?";
+		payDao.executeHql(hql3,new Object[] {sum,cfuid,ccourse});*/
+		/*String hql4="select t.cfpid from Tpay t where cname=? and cfuid=?";
+		List<Tpay> tpl = payDao.find(hql4,new Object[] {ccourse,cfuid});
+		String cfpid = " ";
+		Tpay tp = tpl.get(0);
+		cfpid= tp.getCfpid();
+		System.out.println("cfpid"+cfpid);
+		while (cfpid !=null){
+			String hql="update Tpay tpay set tpay.ccost=(select sum(t.ccost) from Tpay t where t.cfpid=?) where tpay.cid=(select t.cfpid from Tpay t where cid=?)";
+			int e=0;
+			e=payDao.executeHql(hql, cfpid,cfpid);
+			System.out.println("e:"+e);			
+		}*/
+		return null;
+	}	
 }
